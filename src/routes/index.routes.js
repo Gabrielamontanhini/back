31d00b2cdfce7fc19f7d8afade022a9df69a2800
Gabriela Oliveira <gabrielamontanhini@gmail.com>
2023-08-19 19:52:47 -0300

    Fix: adicionada rota sessions

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,10 +2,13 @@ import { Router } from "express";
 import userRouter from "./users.routes.js";
 import postsRouter from "./posts.routes.js";
 import likesRoutes from "./likes.routes.js";
+import sessionsRoutes from "./sessions.routes.js";
 
 const router = Router()
 router.use(userRouter)
+router.use(sessionsRoutes)
 router.use(postsRouter)
 router.use(likesRoutes)
+
 
 export default router
