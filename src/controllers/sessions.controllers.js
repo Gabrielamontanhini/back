1ef49c46ad4e216c942279c1c5651015915c92b4
Gabriela Oliveira <gabrielamontanhini@gmail.com>
2023-08-21 14:32:42 -0300

    FIx: adição de tratamendo de error nas sessions

--- src/controllers/sessions.controllers.js
+++ src/controllers/sessions.controllers.js
@@ -5,8 +5,13 @@ import { getUserByNickname, iniciarSessaoDB } from "../repositories/sessions.rep
 
 export async function postLogin(req, res){
     const {nickname, senha}=req.body
-    const thisUser = await getUserByNickname(nickname)
-    if (thisUser.rowCount == 0) return res.send({ message: "Não encontramos esse usuario!" })
+    try{
+        const thisUser = await getUserByNickname(nickname)
+        if (thisUser.rowCount == 0) return res.send({ message: "Não encontramos esse usuario!" })
+    }
+    catch (err) {
+        return res.status(500).send(nickname)
+    }
     try{
         const hash = bcrypt.hashSync(senha, 10)
         const correctPassword = bcrypt.compare(hash, thisUser.rows[0].senha)
