1cb34b38cd5e1c2513ac172f60ca4e10cc2dd323
Gabriela Oliveira <gabrielamontanhini@gmail.com>
2023-08-21 14:24:40 -0300

    Fix: correção de rota de login

--- src/controllers/sessions.controllers.js
+++ src/controllers/sessions.controllers.js
@@ -5,18 +5,18 @@ import { getUserByNickname } from "../repositories/sessions.repositories.js"
 
 export async function postLogin(req, res){
     const {nickname, senha}=req.body
-    
+    const thisUser = await getUserByNickname(nickname)
+    if (thisUser.rowCount == 0) return res.send({ message: "Não encontramos esse usuario!" })
     try{
-        const thisUser = await getUserByNickname(nickname)
         const hash = bcrypt.hashSync(senha, 10)
-        const correctPassword = bcrypt.compare(hash, thisUser[0].senha)
+        const correctPassword = bcrypt.compare(hash, thisUser.rows[0].senha)
         if (!correctPassword) return res.status(401).send({ message: "Senha incorreta!" })
         const token = uuid()
-        await iniciarSessaoDB(thisUser.id, token)
+        await iniciarSessaoDB(thisUser.rows[0].id, token)
         return res.status(201).send(token)
     }
     catch (err) {
-        res.status(500).send(err.message)
+        res.status(500).send(nickname)
     }
 }
 
