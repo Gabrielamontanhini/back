8247bbd65750e3ec349be9b23b8139ee2bfe46b3
Gabriela Oliveira <gabrielamontanhini@gmail.com>
2023-08-21 14:28:48 -0300

    Fix: adição de return no catch

--- src/controllers/sessions.controllers.js
+++ src/controllers/sessions.controllers.js
@@ -1,6 +1,6 @@
 import { v4 as uuid } from "uuid"
 import bcrypt from "bcrypt"
-import { getUserByNickname } from "../repositories/sessions.repositories.js"
+import { getUserByNickname, iniciarSessaoDB } from "../repositories/sessions.repositories.js"
 
 
 export async function postLogin(req, res){
@@ -16,7 +16,7 @@ export async function postLogin(req, res){
         return res.status(201).send(token)
     }
     catch (err) {
-        res.status(500).send(nickname)
+        return res.status(500).send(nickname)
     }
 }
 
