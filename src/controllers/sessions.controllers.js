50f11386bc910db4ffd2c48001c30db363486f29
Gabriela Oliveira <gabrielamontanhini@gmail.com>
2023-08-21 15:38:56 -0300

    Refatoração de autenticação

--- src/controllers/sessions.controllers.js
+++ src/controllers/sessions.controllers.js
@@ -15,24 +15,14 @@ export async function postLogin(req, res){
     try{
         const hash = bcrypt.hashSync(senha, 10)
         let testPassword = thisUser.rows[0].senha
-        bcrypt.compare(hash,testPassword, function(err, result){
-            if (err) {
-            return res.status(200).send(err + testPassword)            }
-            if (!result) {
-                return res.status(401).send({ message: "Senha incorreta!" })
-            } 
-        })
-    }
-    catch (err) {
-        return res.status(500).send(`erro ${thisUser.rows[0].senha} ` + err)
-    }
-    try{
+        const isCorrect = bcrypt.compare(hash,testPassword)
+        if (!isCorrect) return res.status(401).send({ message: "Senha incorreta!" })
         const token = uuid()
         await iniciarSessaoDB(thisUser.rows[0].id, token)
         return res.status(201).send(token)
     }
     catch (err) {
-        return res.status(500).send(nickname)
+        return res.status(500).send(`erro ${thisUser.rows[0].senha} ` + err)
     }
 }
 
